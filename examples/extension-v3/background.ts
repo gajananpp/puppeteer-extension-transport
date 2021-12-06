6d767a5b1a0df9ceb2449bd258e2dd51d960cc12
gajananpp <patil.gajananps@techforce.ai>
2021-12-06 23:56:46 +0530

    Add v2 and v3 extension examples

---
+++ examples/extension-v3/background.ts
@@ -0,0 +1,42 @@
+import puppeteer from 'puppeteer-core/lib/cjs/puppeteer/web';
+import { ExtensionDebuggerTransport } from '../../lib';
+
+const run = async (tabId: number) => {
+  const extensionTransport = await ExtensionDebuggerTransport.create(tabId);
+  const browser = await puppeteer.connect({
+    transport: extensionTransport,
+    defaultViewport: null,
+  });
+
+  // use first page from pages instead of using browser.newPage()
+  const [page] = await browser.pages();
+
+  await page.goto('https://wikipedia.org');
+
+  const screenshot = await page.screenshot({
+    encoding: 'base64',
+  });
+  console.log(`data:image/png;base64,${screenshot}`);
+
+  const englishButton = await page.waitForSelector('#js-link-box-en > strong');
+  await englishButton?.click();
+
+  const searchBox = await page.waitForSelector('#searchInput');
+  await searchBox?.type('telephone');
+  await page.keyboard.press('Enter');
+
+  await page.close();
+};
+
+chrome.commands.onCommand.addListener(command => {
+  if (command === 'test') {
+    console.log('test')
+    chrome.tabs.create(
+      {
+        active: true,
+        url: 'https://www.google.co.in',
+      },
+      tab => (tab.id ? run(tab.id) : null)
+    );
+  }
+});
